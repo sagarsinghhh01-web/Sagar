8ecd57d69d05f1fa334cfc48d3b19af3e54736cd
sagarsinghhh01-web <sagaraffiliatemarketing01@gmail.com>
2026-05-16 14:31:59 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -46,7 +46,7 @@ export default function HomePage() {
           delay: 1,
         }}
         className="fixed bottom-4 right-4 text-sm text-black/40 pointer-events-none z-50 font-light">
-        @anujbuilds
+        @sagarr
       </motion.div>
     </main>
   )
